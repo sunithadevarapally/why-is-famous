85565406494c71bd65379dc2847c0915daa028da
Sunitha Devarapally <sunitha.konkati@gmail.com>
2022-02-19 01:20:45 -0800

    Added static content to the pages and css changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 
 class Home extends React.Component{
     render(){
         return(
-            <div id = "question">
-                Why is<Form.Control type="" className= "w-50" id="inputPassword5" aria-describedby="passwordHelpBlock"/>famous?
+            <div id = "question-content">
+                <div id = "question">
+                Why is<Form.Control type="" className= "w-50" size="sm" id="inputPassword5" aria-describedby="passwordHelpBlock"/>famous?
+                </div>
+                <Button variant="secondary">Ask</Button>{' '}
             </div>
         );
     }
--- src/components/TopUpvoted.jsx
+++ src/components/TopUpvoted.jsx
@@ -5,9 +5,12 @@ import React from "react";
 class TopUpvoted extends React.Component{
     render(){
         return(
-            <div id = "question">
-                Top upvoted
+            <>
+            <h1>Top Upvoted</h1>
+            <div>
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </div>
+            </>
         );
     }
 
